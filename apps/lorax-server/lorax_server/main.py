5dbb8012ebf0f6518c2d94200c835fe4e71895fb
Christopher Dierkens <cjdierkens@gmail.com>
2024-05-07 12:03:14 -0400

    feat: talk to lorax-client

--- apps/lorax-server/lorax_server/main.py
+++ apps/lorax-server/lorax_server/main.py
@@ -1,9 +1,17 @@
-
 from fastapi import FastAPI
+from lorax import Client
+
+client = Client(
+    base_url="https://serving.app.predibase.com/6dcb0c/deployments/v2/llms/mistral-7b-instruct-dedicated",
+    headers={"Authorization": "Bearer pb_VUEyEdt4WTCpn4Z3G_o3dQ"},
+)
 
 app = FastAPI()
 
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    prompt = "What is your name?"
+    text = client.generate(prompt, max_new_tokens=20, temperature=0.1).generated_text
+
+    return {prompt: "What is your name?", "text": text}
