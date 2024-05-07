4d91653cdbd28476e40ab8af313094406cc423e4
Christopher Dierkens <cjdierkens@gmail.com>
2024-05-07 14:00:57 -0400

    chore: tests pass

--- apps/lorax-web/src/app/app.spec.tsx
+++ apps/lorax-web/src/app/app.spec.tsx
@@ -1,15 +1,15 @@
 import { render } from '@testing-library/react';
-
+import { Provider } from 'react-redux';
 import App from './app';
+import { store } from '../store/store';
 
 describe('App', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(<App />);
+    const { baseElement } = render(
+      <Provider store={store}>
+        <App />
+      </Provider>
+    );
     expect(baseElement).toBeTruthy();
-  });
-
-  it('should have a greeting as the title', () => {
-    const { getByText } = render(<App />);
-    expect(getByText(/Welcome lorax-web/gi)).toBeTruthy();
   });
 });
